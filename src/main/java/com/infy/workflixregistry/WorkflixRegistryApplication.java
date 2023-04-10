16836070e06d2bed9451e607bd337f5c158a3ade
Garen <garengh4@gmail.com>
2023-04-10 15:25:06 -0400

    annotation

--- src/main/java/com/infy/workflixregistry/WorkflixRegistryApplication.java
+++ src/main/java/com/infy/workflixregistry/WorkflixRegistryApplication.java
@@ -2,8 +2,10 @@ package com.infy.workflixregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WorkflixRegistryApplication {
 
 	public static void main(String[] args) {
