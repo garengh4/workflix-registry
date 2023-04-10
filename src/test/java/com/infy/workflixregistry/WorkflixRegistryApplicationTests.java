5a371782daf7aab67c68ffea924e773824692f6e
Garen <garengh4@gmail.com>
2023-04-10 15:22:22 -0400

    init commit

---
+++ src/test/java/com/infy/workflixregistry/WorkflixRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.workflixregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflixRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
